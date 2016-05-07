1646c6c8bd7c7ab5830fb0621a15707e0eeca84d
Robert <robmcarrier@gmail.com>
2016-05-07 15:44:04 -0700

    Done.

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
   $.get("html/sentence.html", function(response) {
     // Here we are getting the element on the page with the id of sentences and
     // insert the response
+
+
+
+
+    
     $("#sentences").html(response);
   });
 });
